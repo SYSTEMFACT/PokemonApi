d5fc9b12cb64f4307e32380925d4dfad975beee5
MARIA MONTOYA <isabelita05981@gmail.coM>
2025-04-28 14:39:54 -0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router'
+import { HashRouter } from 'react-router-dom'
 import App from './routes/App.jsx'
-import { NameProvider } from './contexts/NameContext.jsx'
+
 import './index.css'
+import { NameProvider } from './contexts/NameContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
